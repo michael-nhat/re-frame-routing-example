4ee6ed2ec909c7f48d0409fcdf6598dc3fcb2556
michael-nhat <nhathoang.haiphong@gmail.com>
2023-02-08 22:17:30 +0700

    temp

--- scratch/fetch-api.cljs
+++ scratch/fetch-api.cljs
@@ -14,7 +14,7 @@
    [ajax.core :as ajax]
    [cljs-http.client :as http]
    ;; [cljs.core.async :refer [<!]]
-   [goog.string :as str]
+   [goog.string :as gs]
    [promesa.core :as pr]
    [clojure.core.async :as a]
    [lambdaisland.fetch :as fetch]
@@ -76,7 +76,8 @@
   (atom
    {:a "A" :b "B"}
    :validator
-   (fn [val] (println (str/format "changed: %s" val)) val)))
+   (fn [val] (println (gs/format "changed: %s" val)) val)))
+
 (swap! m1 assoc :a "Aaay")
 
 (ajax/GET
@@ -85,8 +86,9 @@
              "Access-Control-Allow-Origin" "*"}
    :handler handler2
    :error-handler error-handler})
+;; error-handler
 
-(ajax/ajax-request {:uri (str/format "%sproducts/add" d)
+(ajax/ajax-request {:uri (gs/format "%sproducts/add" d)
                   :method :POST
                   :handler handler2
                   :error-handler error-handler
@@ -97,9 +99,9 @@
 ;; wtf this don't work
 ;; it don't work, so, don't try it
 (ajax/POST
-  (str/format
+  (gs/format
    "%sproducts/1" d)
-  {:uri (str/format "%sproducts/add" d)
+  {:uri (gs/format "%sproducts/add" d)
    :handler handler2
    :error-handler error-handler
    :response-format (ajax/json-response-format {:keywords? true})
@@ -121,12 +123,12 @@
 (->
  (pr/promise
   (fn [res rej]
-    ;; (ajax/ajax-request {:url (str/format "%sproducts/1" d)
+    ;; (ajax/ajax-request {:url (gs/format "%sproducts/1" d)
     ;;                   :handler resolve
     ;;                   :error-handler reject
     ;;                   :response-format (ajax/json-response-format {:keywords? true})
     ;;                   :format (ajax/json-request-format)})
-    (ajax/GET (str/format "%sproducts/1" d)
+    (ajax/GET (gs/format "%sproducts/1" d)
             {:headers {"Accept" "application/transit+json"}
              :handler res
              :error-handler rej})))
@@ -137,7 +139,7 @@
  pn/promise
  (fn [res rej]))
 
-(fetch/get (str/format "%sproducts/1" d))
+(fetch/get (gs/format "%sproducts/1" d))
 
 (http/get (api "test")
           {:headers {;; "Access-Control-Allow-Headers" "Content-Type"
@@ -148,11 +150,11 @@
 
 (def l "http://localhost:8081/")
 (defn api [url]
-  (str/format "%s%s" l url))
+  (gs/format "%s%s" l url))
 
 (p/try
   (p/let [resp (fetch/get
-                (str/format "%sproducts/1" d)
+                (gs/format "%sproducts/1" d)
                 {:mode "no-cors"
                  ;; :accept :json
                  ;; :content-type :json
@@ -161,6 +163,7 @@
   (p/catch :default e
      ;; log your exception here
     (prn :error e)))
+
 ;; on js must use res.text => .then(res.json
 ;; p/let return response with p/catch
 (p/try
@@ -198,3 +201,51 @@
                  :body
                  ;; the actual response is a js object, not a clojure map
                  (prn )))))
+(-> (fetch/post "http://localhost:8081/login" {:content-type :json
+                                            :body {:username "nhat"}
+                                            :accept :json
+                                            })
+    (.then (fn [resp]
+             (-> resp
+                 ;; :body
+                 ;; the actual response is a js object, not a clojure map
+                 (print)))))
+
+(url "test")
+(def aa "")
+(def insp (atom ""))
+(swap! a (fn [%] "dkkk"))
+;; this cannot work cannot use js with deref ??
+(js/Object.getOwnPropertyNames @a)
+(js/Object.getOwnPropertyNames (js/Object.assign {} (js->clj @a)))
+(js->clj (js/Object.assign {} (js->clj @a)))
+(utils)
+
+(js/console.log "err: " (str @a) ":endkk")
+(js/Object.entries @a)
+(js/console.log )
+(gs/format "fjas")
+(js/JSON.stringify   @a)
+
+(def ep "http://localhost:8082/")
+
+
+(def insp (atom ""))
+
+(defn set-inspect [val] (swap! insp (fn [_] val)))
+
+(defn default-err-handler [e]
+  "sdkjfls"
+  (set-inspect e)
+  ;; (js/console.log "client error: " (.message e))
+  (js/alert (gs/format "Client error: %s" (.-message e))))
+
+(j-post "login" {:body {:name "nhat"}} (fn [data] (print data)))
+(js/Object.keys (gobj/clone @a))
+
+;; no
+;; good for debug, with many?? (is something still missing?)
+(js/console.log (gobj/getAllPropertyNames @a))
+
+;; write hook in send string repl that 
+;; clear output then dd at the end of buffer could fix that
