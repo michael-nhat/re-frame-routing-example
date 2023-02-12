3b5660f6328b2872f5629303a52c50667b521f09
michael-nhat <nhathoang.haiphong@gmail.com>
2023-02-12 17:36:07 +0700

    dir local

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,15 @@
 (
-    (clojure-mode .
-        (
-            (cider-preferred-build-tool . shadow)
-            ;; (cider-clojure-cli-aliases . ":cider")
-            (cider-default-cljs-repl . shadow)
-            (cider-shadow-watched-builds . 
-                ("app"))
-            (cider-shadow-default-options . "app")
-)))
+ (clojure-mode .
+               ((nh-cider/type . :cljs)
+                (cider-preferred-build-tool . shadow)
+                ;; (cider-clojure-cli-aliases . ":cider")
+                (cider-default-cljs-repl . shadow)
+                (cider-shadow-watched-builds . ("app"))
+                (cider-shadow-default-options . "app")))
+ (nil .
+               ((nh-cider/type . :cljs)
+                (cider-preferred-build-tool . shadow)
+                ;; (cider-clojure-cli-aliases . ":cider")
+                (cider-default-cljs-repl . shadow)
+                (cider-shadow-watched-builds . ("app"))
+                (cider-shadow-default-options . "app"))))
