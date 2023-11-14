86e61d2e32bc4e1656a2a76b875538b5a01e86d5
michael-nhat <nhathoang.haiphong@gmail.com>
2023-11-14 20:04:50 +0700

    local var

--- .dir-locals.el
+++ .dir-locals.el
@@ -7,4 +7,13 @@
             (cider-shadow-watched-builds . 
                 ("app"))
             (cider-shadow-default-options . "app")
-)))
+))
+    (nil .
+                  (
+                   (cider-preferred-build-tool . shadow)
+                   ;; (cider-clojure-cli-aliases . ":cider")
+                   (cider-default-cljs-repl . shadow)
+                   (cider-shadow-watched-builds . 
+                                                ("app"))
+                   (cider-shadow-default-options . "app")
+                   )))
